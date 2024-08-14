8b543fef67146a1bc6ae14b017cb98b08f11960a
Murilo Andrade <murilo.andrade@gmail.com>
2024-08-13 23:04:31 -0300

    Apply modifications suggested by Android Studio

--- app/src/main/java/br/com/sandiego/netdroid/HomeActivity.java
+++ app/src/main/java/br/com/sandiego/netdroid/HomeActivity.java
@@ -1,7 +1,7 @@
 package br.com.sandiego.netdroid;
 
 import android.os.Bundle;
-import android.view.View;
+//import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -20,23 +20,17 @@ import br.com.sandiego.netdroid.databinding.ActivityHomeBinding;
 public class HomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        br.com.sandiego.netdroid.databinding.ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarHome.toolbar);
-        binding.appBarHome.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        binding.appBarHome.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
